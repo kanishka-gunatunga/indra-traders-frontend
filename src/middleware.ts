7f417c8c7e0cdd2c53ef575220b5a5ab94f93533
Kaveesha Sulakshana <sulakshanakaveesha@gmail.com>
2025-11-28 13:53:49 +0530

    role based access

--- src/middleware.ts
+++ src/middleware.ts
@@ -95,8 +95,8 @@ export const config = {
     // ],
 
     matcher: [
-        // "/((?!_next/static|_next/image|favicon.ico|api/|.*\\.(?:png|jpg|svg)).*)",
-        "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:png|jpg|svg)).*)",
+        "/((?!_next/static|_next/image|favicon.ico|api/|.*\\.(?:png|jpg|svg)).*)",
+        // "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:png|jpg|svg)).*)",
     ],
 };
 
